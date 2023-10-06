536f3138c1f5b2045e90af54bc69c8f5f5298ee3
KauaMB2 <i.kauamoreirabatista@gmail.com>
2023-10-06 20:13:44 -0300

    ButtonAndConditionalFormatting

--- App.js
+++ App.js
@@ -1,123 +1,34 @@
-import { View, Text, StyleSheet } from 'react-native';
-
-export default function App() {
+import { View, Text, StyleSheet, Button } from 'react-native';
+import {useState} from "react"
+const App = () => {
+  const [isConditionMet, setIsConditionMet]=useState(true)
   return (
-    <View style={styles.main}>
-      <Text style={styles.textStyle1}>
-        flexDirection: "row"
-      </Text>
-      <View style={styles.box}>
-        <Text style={styles.textStyle2}>
-          justifyContent: "flex-start"
-        </Text>
-        <View style={styles.bigContainer1}>
-          <Text style={[styles.simpleContainer, styles.container1]}>1</Text>
-          <Text style={[styles.simpleContainer, styles.container2]}>2</Text>
-          <Text style={[styles.simpleContainer, styles.container3]}>3</Text>
-        </View>
-      </View>
-      <View style={styles.box}>
-        <Text style={styles.textStyle2}>
-          justifyContent: "flex-end"
-        </Text>
-        <View style={styles.bigContainer2}>
-          <Text style={[styles.simpleContainer, styles.container1]}>1</Text>
-          <Text style={[styles.simpleContainer, styles.container2]}>2</Text>
-          <Text style={[styles.simpleContainer, styles.container3]}>3</Text>
-        </View>
-      </View>
-      <View style={styles.box}>
-        <Text style={styles.textStyle2}>
-          justifyContent: "space-between"
-        </Text>
-        <View style={styles.bigContainer3}>
-          <Text style={[styles.simpleContainer, styles.container1]}>1</Text>
-          <Text style={[styles.simpleContainer, styles.container2]}>2</Text>
-          <Text style={[styles.simpleContainer, styles.container3]}>3</Text>
-        </View>
-      </View>
-      <View style={styles.box}>
-        <Text style={styles.textStyle2}>
-          justifyContent: "space-around"
-        </Text>
-        <View style={styles.bigContainer4}>
-          <Text style={[styles.simpleContainer, styles.container1]}>1</Text>
-          <Text style={[styles.simpleContainer, styles.container2]}>2</Text>
-          <Text style={[styles.simpleContainer, styles.container3]}>3</Text>
-        </View>
-      </View>
-      <View style={styles.box}>
-        <Text style={styles.textStyle2}>
-          justifyContent: "center"
-        </Text>
-        <View style={styles.bigContainer5}>
-          <Text style={[styles.simpleContainer, styles.container1]}>1</Text>
-          <Text style={[styles.simpleContainer, styles.container2]}>2</Text>
-          <Text style={[styles.simpleContainer, styles.container3]}>3</Text>
-        </View>
+    <View style={styles.container}>
+      {/* Use a ternary operator to conditionally format the component */}
+      {isConditionMet ? (
+        <Text style={styles.text}>Condition is met</Text>
+      ) : (
+        <Text style={[styles.text, styles.errorText]}>Condition is not met</Text>
+      )}
+      <View>
+        <Button title='Click here' onPress={()=>{setIsConditionMet(!isConditionMet)}}></Button>
       </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  main:{
-    alignItems: "center",
-  },
-  box:{
-    width: "80%",
-    borderColor: "purple",
-    borderWidth: 1,
-    backgroundColor: "black",
-    marginBottom: 10,
-  },
-  textStyle1:{
-    fontWeight: "bold",
-    fontSize: 15,
-    color: "black",
-  },
-  textStyle2:{
-    fontWeight: "bold",
-    fontSize: 10,
-    color: "white",
-  },
-  simpleContainer:{
-    width: 30,
-    height: 30,
-    color: "white",
-  },
-  bigContainer1:{
+  container: {
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "start",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  bigContainer2:{
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "end",
+  text: {
+    fontSize: 24,
   },
-  bigContainer3:{
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  bigContainer4:{
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-around",
-  },
-  bigContainer5:{
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "center",
-  },
-  container1:{
-    backgroundColor: "#00faaa",
-  },
-  container2:{
-    backgroundColor: "#00daaa",
-  },
-  container3:{
-    backgroundColor: "#00aaaa",
+  errorText: {
+    color: 'red', // Apply a different style for error condition
   },
 })
+
+export default App
---
+++ README.md
@@ -0,0 +1,19 @@
+# React Native Playlist Documents
+
+Welcome to the React Native Playlist Documents repository! This repository is dedicated to storing and organizing documents, tutorials, and resources related to the React Native playlist.
+
+## Overview
+
+- **What is React Native?**
+  React Native is an open-source JavaScript framework for building mobile applications. It allows developers to use React (a JavaScript library for building user interfaces) to create mobile apps that run on both iOS and Android platforms.
+
+- **Playlist Content**
+  This repository contains a collection of documents and resources related to the React Native playlist. It includes tutorials, code samples, and other educational materials to help you learn and work with React Native.
+
+## Getting Started
+
+If you're new to React Native or this playlist, here are some steps to get started:
+
+1. **Clone the Repository**
+   Clone this repository to your local machine using the following command:
+
