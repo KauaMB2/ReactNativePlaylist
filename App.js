974127209ceedead4b7b580abab6b92a62dfb64d
KauaMB2 <i.kauamoreirabatista@gmail.com>
2023-10-07 17:11:26 -0300

    TouchableOpacityAndTouchableHightLight

--- App.js
+++ App.js
@@ -1,22 +1,46 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-} from 'react-native'
-import ModalComponent from './components/ModalComponent'
-const App = () => {
+import { StyleSheet,
+         Text, 
+         View, 
+         TouchableHighlight,
+         TouchableOpacity } from 'react-native';
+import {useState} from 'react'
+
+
+export default function App() {
+  const [count,setCount]=useState(0)
   return (
-    <ModalComponent/>
+    <View style={styles.container}>
+      <Text>Counter: {count}</Text>
+      <TouchableHighlight onPress={()=>{setCount(count+1)}}
+        style={styles.touchable}
+        underlayColor="red"
+      >
+        <Text style={styles.text}>Click Me!</Text>
+      </TouchableHighlight>
+      <TouchableOpacity style={styles.touchable} onPress={()=>{setCount(count+1)}}>
+        <Text style={styles.text}>Click Me!</Text>
+      </TouchableOpacity>
+    </View>
   )
 }
-
+  
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#fff',
     alignItems: 'center',
+    justifyContent: 'center',
   },
+  touchable: {
+    height: 50,
+    width: 200,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#4287f5',
+    marginBottom: 10,
+  },
+  text: {
+    color: "#fff"
+  }
 })
-
-export default App
