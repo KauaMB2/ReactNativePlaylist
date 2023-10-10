77544fd477eceb429ad41067164ed9e0a34673d4
KauaMB2 <i.kauamoreirabatista@gmail.com>
2023-10-10 18:15:18 -0300

    RefreshControl

--- App.js
+++ App.js
@@ -1,17 +1,68 @@
-import {View, Text, StyleSheet} from 'react-native'
-import SwitchButton from './components/SwitchButton'
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <SwitchButton/>
-      <SwitchButton/>
-    </View>
-  )
-}
-const styles=StyleSheet.create({
-  container:{
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+import { ScrollView, View, Text, StyleSheet, RefreshControl } from 'react-native'
+import {useState} from 'react'
+const App = () => {
+  const [updating,setUpdate]=useState(false)
+  const update=()=>{
+    setUpdate(true)
+    setTimeout(()=>{setUpdate(false)},3000)
   }
-})
+  return (
+    //RefreshControl is the refresh loading circle that appears above that shows the page is beeing loaded
+    <ScrollView
+    contentContainerStyle={styles.scrollView}
+    refreshControl={<RefreshControl refreshing={updating} onRefresh={update}/>}>
+      <View style={styles.container}>
+        <Text style={styles.text}>Item 1</Text>
+        <Text style={styles.text}>Item 2</Text>
+        <Text style={styles.text}>Item 3</Text>
+        <Text style={styles.text}>Item 4</Text>
+        <Text style={styles.text}>Item 5</Text>
+        <Text style={styles.text}>Item 6</Text>
+        <Text style={styles.text}>Item 7</Text>
+        <Text style={styles.text}>Item 8</Text>
+        <Text style={styles.text}>Item 1</Text>
+        <Text style={styles.text}>Item 2</Text>
+        <Text style={styles.text}>Item 3</Text>
+        <Text style={styles.text}>Item 4</Text>
+        <Text style={styles.text}>Item 5</Text>
+        <Text style={styles.text}>Item 6</Text>
+        <Text style={styles.text}>Item 7</Text>
+        <Text style={styles.text}>Item 8</Text>
+        <Text style={styles.text}>Item 1</Text>
+        <Text style={styles.text}>Item 2</Text>
+        <Text style={styles.text}>Item 3</Text>
+        <Text style={styles.text}>Item 4</Text>
+        <Text style={styles.text}>Item 5</Text>
+        <Text style={styles.text}>Item 6</Text>
+        <Text style={styles.text}>Item 7</Text>
+        <Text style={styles.text}>Item 8</Text>
+        <Text style={styles.text}>Item 1</Text>
+        <Text style={styles.text}>Item 2</Text>
+        <Text style={styles.text}>Item 3</Text>
+        <Text style={styles.text}>Item 4</Text>
+        <Text style={styles.text}>Item 5</Text>
+        <Text style={styles.text}>Item 6</Text>
+        <Text style={styles.text}>Item 7</Text>
+        <Text style={styles.text}>Item 8</Text>
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    flexGrow: 1,
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 20,
+  },
+  text: {
+    fontSize: 24,
+    marginBottom: 10,
+  },
+});
+
+export default App;
