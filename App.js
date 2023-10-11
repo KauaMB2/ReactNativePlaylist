65a32c4d3021f924f4035ff7ca2fd13f31486aae
KauaMB2 <i.kauamoreirabatista@gmail.com>
2023-10-11 07:01:02 -0300

    StackNavigator

--- App.js
+++ App.js
@@ -1,35 +1,86 @@
-import { View, Text, StyleSheet, StatusBar,Button } from 'react-native'
-import {useState} from 'react'
-const App = () => {
-  const [color,setColor]=useState("")
+import { View, Text, StyleSheet, Button } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+const Stack = createStackNavigator()
+
+const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <StatusBar
-      backgroundColor={color}
-      barStyle="default"
-      hidden={false}
-      animated={true}
-      translucent={false}
-      />
+      <Text>Home Screen</Text>
       <Text>CFB Cursos</Text>
-      <Text>React Native Course</Text>
-      <Button title='Red' color="red" onPress={()=>{setColor("red")}}/>
-      <Button title='Green' color="green" onPress={()=>{setColor("green")}}/>
-      <Button title='Blue' color="blue" onPress={()=>{setColor("blue")}}/>
+      <Button title="Chanel screen" color="red" onPress={() => navigation.navigate("Chanel")} />
+      <Button title="Courses screen" color="red" onPress={() => navigation.navigate("Courses")} />
     </View>
   )
 }
 
+const ChanelScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Chanel Screen</Text>
+      <Text>CFB Cursos</Text>
+      <Button title="Home" color="red" onPress={() => navigation.navigate("Home")} />
+    </View>
+  )
+}
+
+const CoursesScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Click here to see about React Native course:</Text>
+      <Button title="React Native" color="red" onPress={() => navigation.navigate("ReactNativeCourse", { lessons: 40, creator: "Kauã" })} />
+    </View>
+  )
+}
+
+const ReactNativeCourse = ({ route, navigation }) => {
+  const { lessons, creator } = route.params
+
+  return (
+    <View style={styles.container}>
+      <Text>React Native!</Text>
+      <Text>Lessons: {lessons}</Text>
+      <Text>Creator: {creator}</Text>
+      <Button title="Home" color="red" onPress={() => navigation.navigate("Home")} />
+      <Button title="Come back to courses" color="red" onPress={() => navigation.goBack()} />
+    </View>
+  )
+}
+
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name="Home" component={HomeScreen}
+        options={{
+          title: "Home screen",
+          headerStyle: {
+            backgroundColor:"#008"
+          },
+          headerTintColor:"#fff",
+          headerRight:()=>(
+            <Button
+            title="Cursos"
+            color="#000"
+            onPress={()=>{alert("Test button")}}
+            />
+          )
+        }}/>
+        <Stack.Screen name="Chanel" component={ChanelScreen} options={{ title: "Chanel screen" }} />
+        <Stack.Screen name="Courses" component={CoursesScreen} options={{ title: "Courses screen" }} />
+        <Stack.Screen name="ReactNativeCourse" component={ReactNativeCourse} options={{ title: "React Native screen" }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
 const styles = StyleSheet.create({
-  scrollView: {
-    flexGrow: 1,
-  },
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 20,
+    justifyContent: "center",
+    alignItems: "center",
   }
 })
 
-export default App;
+export default App
