0b199a845b72a622d878b4b10316df12544bc454
KauaMB2 <i.kauamoreirabatista@gmail.com>
2023-10-12 13:11:50 -0300

    AsyncStorage

--- App.js
+++ App.js
@@ -1,85 +1,62 @@
-import { SafeAreaView, Alert, StyleSheet } from 'react-native'
-import { useState } from "react"
-import Gasoline from "./components/Gasoline"
-import Ethanol from "./components/Ethanol"
-import BtnCalc from './components/BtnCalc'
-import Result from './components/Result'
+import { SafeAreaView, Text, StyleSheet } from 'react-native'
+import { useState, useEffect } from 'react'
+import AsyncStorage from '@react-native-community/async-storage'
 
 const App = () => {
-  const [gasoline,setGasoline]=useState(0)
-  const [ethanol, setEthanol]=useState(0)
-  const [result, setResult]=useState("")
-  const calc=()=>{
-    if(!gasoline || gasoline==0){
-      Alert.alert("ALERTA!", "Informe o preço da gasolina.")
-      return
+  const [course, setCourse] = useState(null)
+  const [config, setConfig] = useState("white")
+
+  useEffect(() => {
+    async function setupAsyncStorage() {
+      // Store data
+      await Storage('01', 'React Native')
+      await Storage('02', 'Javascript')
+      await Storage('03', 'C++')
+      await Storage('04', 'HTML + CSS')
+      await Storage('05', 'HTML')
+
+      // Retrieve data
+      const searchedCourse = await Search('04')
+      setCourse(searchedCourse)
+
+      // Remove an item
+      await AsyncStorage_Courses.removeItem('03')
+
+      // Set background color
+      setStorageConfig('backGColor', 'white')
     }
-    if(!ethanol || ethanol==0){
-      Alert.alert("ALERTA!", "Informe o preço do etanol.")
-      return
-    }
-    let res
-    let calc=((ethanol/gasoline)*100).toFixed(1)
-    if(calc>70){
-      res="Gasolina"
-    }else{
-      res="Etanol"
-    }
-    setResult(res)
-    Alert.alert("Resultado",`O etanol está custando ${calc}% da gasolina. Portanto, portanto, é melhor abastecer com ${res}`)
+
+    setupAsyncStorage()
+  }, [])
+
+  const Storage = async (key, value) => {
+    await AsyncStorage.setItem(key, value)
   }
-  const clear=()=>{
-    setResult('')
+
+  const Search = async (key) => {
+    const value = await AsyncStorage.getItem(key)
+    return value
   }
-  const settingGasoline=(value)=>{
-    clear()
-    setGasoline(value)
+
+  const setStorageConfig = (key, value) => {
+    AsyncStorage.setItem(key, value)
+    setConfig(value)
   }
-  const settingEthanol=(value)=>{
-    clear()
-    setEthanol(value)
-  }
+
   return (
-    <SafeAreaView style={styles.container}>
-      <Gasoline styles={styles} settingGasoline={settingGasoline}/>
-      <Ethanol styles={styles} settingEthanol={settingEthanol}/>
-      <BtnCalc styles={styles} calc={calc}/>
-      <Result styles={styles} result={result}/>
+    <SafeAreaView style={[styles.container, { backgroundColor: config }]}>
+      <Text>Armazenamento local / Async-Storage</Text>
+      <Text>Curso: {course}</Text>
+      <Text style={{ color: '#444' }}>Configurações: {config}</Text>
     </SafeAreaView>
   )
 }
-
 const styles = StyleSheet.create({
-  body: {
-    padding: 10
-  },
-  input: {
-    width: '100%',
-    borderWidth: 1,
-    borderColor: "#000",
-    padding: 10,
-    borderRadius: 10
-  },
-  btnTxt: {
-    textTransform: 'uppercase',
-    fontSize: 20,
-    color: "white"
-  },
-  btn:{
-    backgroundColor: '#b00',
-    justifyContent: "center",
-    alignItems: "center",
-    fontSize: 20,
-    padding: 15
-  },
   container: {
-    padding: 10,
-    paddingTop: 20
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  centerImg:{
-    justifyContent: "center",
-    alignItems: "center"
-  }
 })
 
 export default App
--- styles/style.js
+++ styles/style.js
@@ -5,13 +5,5 @@ export default StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-    },
-    item:{
-        borderWidth: 1,
-        borderRadius: 10,
-        borderColor: "green",
-        padding: 20,
-        fontSize: 18,
-        marginBottom: 20,
     }
 })
